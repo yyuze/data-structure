5884072bdc0cabdd2ae1dd21bdd91d35118e3c72
yyuze <sobeautiful@126.com>
2018-12-11 19:22:42 +0800

    fixed sl bug

--- LinearList/src/cpp/com/yyuze/sl/main.cpp
+++ LinearList/src/cpp/com/yyuze/sl/main.cpp
@@ -2,7 +2,7 @@
 #include<iostream>
 
 using namespace std;
-int main() {
+int main() {	
 	SequentialList<int>* list = new SequentialList<int>();
 	cout << "insert 10 elements in list" << endl;
 	for (int i = 0; i < 10; i++) {
@@ -11,15 +11,17 @@ int main() {
 
 	cout << "now length of list is " << list->getLength() << endl;
 	
-	cout << "the 3rd element is " << list->getElelment(3) << endl;
+	cout << "the 4th element is " << list->getElelment(3) << endl;
 	
 	cout << "first index of element \"5\" is " << list->getFirstIndexOf(5)<< endl;
 	
 	cout << "remove element \"5\"" << endl;
 	list->remove(list->getElelment(list->getFirstIndexOf(5)));
 
-	cout << "current element in list is :" << endl;
+	cout << "current element in list is :" << endl; 
 	list->traverse();
+
+	list->insert(20,7);
 
 	cout << "list scend sorted" << endl;
 	list->sort();
@@ -27,6 +29,6 @@ int main() {
 	cout << "get a sorted cloned list:" << endl;
 	SequentialList<int>& clist = list->clone();
 	clist.traverse();
-
+	
 	return 0;
 }
--- LinearList/src/cpp/com/yyuze/sl/sequential_list.h
+++ LinearList/src/cpp/com/yyuze/sl/sequential_list.h
@@ -1,6 +1,7 @@
 #ifndef LINEARLIST_CPP_COM_YYUZE_SL_SEQUENTIALLIST_H_
 #define LINEARLIST_CPP_COM_YYUZE_SL_SEQUENTIALLIST_H_
-
+#include<iostream>
+using namespace std;
 template<class T>
 class SequentialList
 {
@@ -16,26 +17,85 @@ public:
 	~SequentialList() 
 	{
 		this->clearList();
-		free(this->addr);
+	} 
+
+	void clearList() {
+		for (int i = 0; i < this->length; ++i) {
+			*(this->addr + i)=0;
+		}
 	}
 
-	void clearList();
+	int getLength() {
+		return this->length;
+	}
 
-	int getLength();
+	T& getElelment(int index) {
+		return *(this->addr + index);
+	}
 
-	T& getElelment(int index);
+	int getFirstIndexOf(T element) {
+		for (int i = 0; i < this->length; ++i) {
+			if (element == *(this->addr + i)) {
+				return i;
+			}
+		}
+		return -1;
+	}
 
-	int getFirstIndexOf(T element);
+	bool insert(T element, int index) {
+		if (index > this->length) {
+			index = this->length;
+		}
+		T temp = *(this->addr + index);
+		for (int i = index; i < this->length; ++i) {
+			T t = *(this->addr + i + 1);
+			*(this->addr + i + 1 )= temp;
+			temp = t;
+		}
+		*(this->addr + index) = element;
+		++this->length;
+		return true;
+	}
 
-	bool insert(T element,int index);
+	bool remove(T& element) {
+		int position = &element - this->addr;
+		for (int i = position; i < this->length - 1; ++i) {
+			*(this->addr + i) = *(this->addr + i + 1);
+		}
+		--this->length;
+		return true;
+	}
 
-	bool remove(T& element);
-
-	void traverse();
+	void traverse() {
+		for (int i = 0; i < this->length; ++i) {
+			cout << *(this->addr + i )<< endl;
+		}
+	}
 	
-	SequentialList<T>& clone();
+	SequentialList<T>& clone() {
+		SequentialList<T>* newList = new SequentialList<T>();
+		for (int i = 0; i < this->length; i++) {
+			newList->insert(*(this->addr + i), i);
+		}
+		return *newList;
+	}
 
-	void sort();
+	void sort() {
+		for (int i = 0; i < this->length; ++i) {
+			bool sortedFlag = true;
+			for (int j = 0; j < length - i - 1; ++j) {
+				if (*(this->addr + j) > *(this->addr + j + 1)) {
+					T temp = *(this->addr + j + 1); 
+					*(this->addr + j + 1) = *(this->addr + j);
+					*(this->addr + j) = temp;
+					sortedFlag = false;
+				}
+			}
+			if (sortedFlag) {
+				return;
+			}
+		}
+	}
 };
 
 
